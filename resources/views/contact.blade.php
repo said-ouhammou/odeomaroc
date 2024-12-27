db94e6491e5adb5c5d729d1356f1e90c4d6a5ca4
yassineahlouch <yassineahlouch@gmail.com>
2024-12-27 13:44:18 +0100

    cnt

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -39,8 +39,8 @@
                             <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
                             <input type="email" id="email" name="email" required
                                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#800020]">
-                                <div>
-                                    <div class="style:none;">
+                                <div style="display:none">
+                                    <div style="display:none">
                                         <input type="text" name="honypot" value="">
                                     </div>
                                 </div>

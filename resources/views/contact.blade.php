5f1ca1b3ed25a7ce89f1a9aa6c75fe3e670660da
yassineahlouch <yassineahlouch@gmail.com>
2024-12-27 13:11:38 +0100

    contact add honypot detect bot spam and ip adresse Log

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -39,6 +39,11 @@
                             <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
                             <input type="email" id="email" name="email" required
                                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#800020]">
+                                <div>
+                                    <div class="style:none;">
+                                        <input type="text" name="honypot" value="">
+                                    </div>
+                                </div>
                         </div>
                         <div class="mb-4">
                             <label for="phone" class="block text-gray-700 font-medium mb-2">Téléphone:</label>
